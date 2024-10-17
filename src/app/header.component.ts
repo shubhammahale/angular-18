2c1f54f19267a5d0d4ea0c8e9d640c0b69189e7f
Mahale <smahale@netapp.com>
2024-10-17 19:30:01 +0530

    intial commit

---
+++ src/app/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-header',
+    templateUrl:'./header.component.html',
+    imports:[],
+    standalone:true,
+    styleUrl: './header.component.css',
+})
+export class HeaderComponent{
+
+}
