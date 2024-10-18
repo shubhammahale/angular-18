07a284163bcdb00a8869e87f799377f2a2055772
Mahale <smahale@netapp.com>
2024-10-19 00:34:42 +0530

    adding task component and types

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,6 +8,17 @@ import {
   Output,
 } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -16,9 +27,7 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
 
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
@@ -29,10 +38,10 @@ export class UserComponent {
   // imagePath = computed(() => 'assets/users/' + this.avatar());
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
