0d8b8302f3224eafb3bd966aaee9050de2699aea
Mahale <smahale@netapp.com>
2024-10-21 01:08:58 +0530

    delete task and add classes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -18,7 +18,7 @@ import { User } from './user.model';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
-
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
 
