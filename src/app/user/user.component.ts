60a486130a688f0d10a850229a391cd30dc7f5d8
Mahale <smahale@netapp.com>
2024-10-23 16:00:57 +0530

    content projection with ng-content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,11 +8,12 @@ import {
   Output,
 } from '@angular/core';
 import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
